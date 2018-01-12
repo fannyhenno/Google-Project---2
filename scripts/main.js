e3a33669c3b5e209a6299885ba705011c32dbb0f
fanny henno <fannyhenno@gmail.com>
2018-01-12 02:22:52 +0100

    gros commit

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+document.getElementById("central").style.display = "none";
+
+function myFunction() {
+	document.getElementById("central").style.display = "block";
+	document.getElementById("bouton").style.display = "none";
+}
+
+function mafonction() {
+	let recherchegoogle = document.getElementById("texte").value;
+document.location.href = "https://www.google.fr/#q="+recherchegoogle;
+}
+
+setTimeout(function() {alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");}, 10000);
+
+
